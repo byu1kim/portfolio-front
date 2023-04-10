a997ee834a57fd864bf5351c591b6588966baf8e
Byul Kim <caxbelle@gmail.com>
2023-04-10 04:15:57 -0700

    data

---
+++ src/data/temp.js
@@ -0,0 +1,196 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Pick and Go",
+    summary: "BCIT Internal project with ASP .NET",
+    detail:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum porro sed alias dolorem repudiandae expedita cum dolor deserunt? Molestiae iure voluptatem natus neque error quaerat aliquam ipsam voluptates possimus hic",
+    website: "https://byultube.onrender.com/",
+    github: "https://github.com/kchong30/pick-and-go",
+    thumb: "https://i.imgur.com/Iomj6SN.png",
+    images: [
+      "https://i.imgur.com/zw3OMaI.png",
+      "https://i.imgur.com/bnyUmAP.png",
+      "https://i.imgur.com/1iHLItU.png",
+      "https://i.imgur.com/0DVlWu0.png",
+      "https://i.imgur.com/j6DoLgx.png",
+      "https://i.imgur.com/CPxdAAr.png",
+      "https://i.imgur.com/YKS7oo7.png",
+      "https://i.imgur.com/BcaBgFZ.png",
+    ],
+    categories: [
+      { id: 2, name: "frontend" },
+      { id: 4, name: "school project" },
+    ],
+    tags: [
+      { id: 3, name: "ASP.Net" },
+      { id: 7, name: "jQuery" },
+      { id: 8, name: "SQL" },
+      { id: 12, name: "Bootstrap" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Movie App",
+    summary: "Frontend application is built with React and uses a movie API to retrieve and display movie data.",
+    detail:
+      "The frontend application was built using the React JavaScript library. It interfaces with a movie API to retrieve data and display it to users. This means that users can interact with the application to search for movies, view movie details, and potentially perform other actions related to movies such as favourite button. The application likely uses modern web development techniques such as asynchronous data fetching, responsive design, and state management to provide a smooth and intuitive user experience. Overall, the application offers a convenient and visually appealing way to browse and interact with movie data.",
+    website: "https://byu1kim.github.io/react-movie-app",
+    github: "https://github.com/byu1kim/react-movie-app",
+    thumb: "https://i.imgur.com/n3kxE7s.png",
+    images: [
+      "https://i.imgur.com/8L2FnyK.jpg",
+      "https://i.imgur.com/c56M1kX.jpg",
+      "https://i.imgur.com/h6Azhf7.jpg",
+      "https://i.imgur.com/5BTkU6y.jpg",
+    ],
+    tags: [
+      { id: 6, name: "React" },
+      { id: 10, name: "CSS" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Byul Chat",
+    summary: "Social Media Service built with React, Amazon Cognito, Amazon API Gateway and Lambda function",
+    detail:
+      "<p>Socical Media Service application which users can interact with others. It has a login system that uses Amazon Cognito for authentication. Only logged-in users can create new posts, and they can interact with other users by adding comments to their posts.</p> <p>The application utilizes the React library for the front-end, which allows for dynamic user interfaces and smooth interactions. Amazon Labmda function and API gateway are used for the server-less back-end, which provides a scalable and efficient server environment for handling data and processing user requests.</p> <p>Overall, it provides a secure and engaging social media experience for users, enabling them to create and share content with others in a controlled and secure environment.</p>",
+    website: "https://melodious-maamoul-b42885.netlify.app/",
+    github: "https://github.com/byu1kim/sns",
+    thumb: "https://i.imgur.com/MBArd5I.png",
+    images: [
+      "https://i.imgur.com/MBArd5I.png",
+      "https://i.imgur.com/iDUPD58.png",
+      "https://i.imgur.com/KfWJPCx.png",
+      "https://i.imgur.com/EZmHJ25.png",
+      "https://i.imgur.com/LMObLF8.png",
+    ],
+    tags: [
+      { id: 2, name: "Node.js" },
+      { id: 6, name: "React" },
+      { id: 8, name: "SQL" },
+      { id: 13, name: "Tailwind" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Byul ChatPT",
+    summary:
+      "AWS CLI API Gateway and Lambda function for the backend, React for the frontend, and enables users to ask questions and receive responses from an AI.",
+    detail:
+      "<p>It allows users to add questions and send messages to an AI. The backend of the app is built using AWS CLI API Gateway and Lambda function, which allows for efficient and scalable processing of user requests.</p> <p>The frontend of the app is built using React, which enables dynamic and responsive user interfaces. Users can add questions and send messages to the AI through a simple and intuitive interface. The backend processes the user requests and sends back responses to the frontend, which then displays them to the user.</p>",
+    website: "https://melodious-maamoul-b42885.netlify.app/",
+    github: "https://github.com/byu1kim/sns",
+    thumb: "https://i.imgur.com/WjpRnM2.png",
+    images: ["https://i.imgur.com/WjpRnM2.png", "https://i.imgur.com/FaC6yI9.png"],
+    tags: [
+      { id: 2, name: "Node.js" },
+      { id: 6, name: "React" },
+      { id: 8, name: "SQL" },
+      { id: 13, name: "Tailwind" },
+    ],
+  },
+  {
+    id: 5,
+    title: "Year Book",
+    summary: "Social Media Service built with React, Amazon Cognito, Amazon API Gateway and Lambda function",
+    detail:
+      "<p>It allows users to add questions and send messages to an AI. The backend of the app is built using AWS CLI API Gateway and Lambda function, which allows for efficient and scalable processing of user requests.</p> <p>The frontend of the app is built using React, which enables dynamic and responsive user interfaces. Users can add questions and send messages to the AI through a simple and intuitive interface. The backend processes the user requests and sends back responses to the frontend, which then displays them to the user.</p>",
+    website: null,
+    github: "https://github.com/byu1kim/node-yearbook",
+    thumb: "https://i.imgur.com/4F1PbNt.png",
+    images: ["https://i.imgur.com/4F1PbNt.png", "https://i.imgur.com/W3EjTgc.png", "https://i.imgur.com/xw6sjGf.png"],
+    tags: [
+      { id: 2, name: "Node.js" },
+      { id: 9, name: "MongoDB" },
+      { id: 10, name: "CSS" },
+    ],
+  },
+  {
+    id: 6,
+    title: "Web Scrapping",
+
+    summary: "Python web scrapping application",
+    detail: "<p>Web scrapping with Python Flask.</p>",
+    website: null,
+    github: null,
+    thumb: "https://i.imgur.com/Mq9ybfQ.jpg",
+    images: ["https://i.imgur.com/Mq9ybfQ.jpg", "https://i.imgur.com/UItDBeT.jpg"],
+
+    tags: [{ id: 4, name: "Python" }],
+  },
+  {
+    id: 7,
+    title: "Byultube",
+    summary: "Youtube clone built by Node.js",
+    detail:
+      "a YouTube clone app with Node.js, Sass, and Pug can be created with user authentication, video and image uploads, video recording, video and comment CRUD, and search functionality. The app can use Express and Passport for authentication, multer for file uploads, MongoDB for data storage, and Elasticsearch for search indexing. Styling can be done with Sass and templates with Pug, and a front-end framework like React or Vue.js can enhance the user experience. Overall, the app provides similar features to the real YouTube platform, making it a challenging but rewarding project.",
+    website: "https://byultube.onrender.com/",
+    github: "https://github.com/byu1kim/byultube",
+    thumb: "https://i.imgur.com/OW34IdM.png",
+    images: [
+      "https://i.imgur.com/RocJSmm.png",
+      "https://i.imgur.com/7ffc8xL.png",
+      "https://i.imgur.com/OW34IdM.png",
+      "https://i.imgur.com/M5fl5mb.png",
+      "https://i.imgur.com/cX7XICJ.png",
+    ],
+    categories: [
+      { id: 2, name: "frontend" },
+      { id: 4, name: "school project" },
+    ],
+    tags: [
+      { id: 1, name: "Javascript" },
+      { id: 2, name: "Node.js" },
+      { id: 9, name: "MongoDB" },
+      { id: 11, name: "Sass" },
+    ],
+  },
+];
+
+// EJS, PUG, HTML?, AWS??, GULP?, WEBPACK?
+export const tags = [
+  { id: 1, name: "Javascript" },
+  { id: 2, name: "Node.js" },
+  { id: 3, name: "ASP.Net" },
+  { id: 4, name: "Python" },
+  { id: 5, name: "PHP" },
+  { id: 6, name: "React" },
+  { id: 7, name: "jQuery" },
+  { id: 8, name: "SQL" },
+  { id: 9, name: "MongoDB" },
+  { id: 10, name: "CSS" },
+  { id: 11, name: "Sass" },
+  { id: 12, name: "Bootstrap" },
+  { id: 13, name: "Tailwind" },
+];
+
+// full stack, frontend, backend, school project, individual project, industry project
+export const categories = [
+  { id: 1, name: "full stack" },
+  { id: 2, name: "frontend" },
+  { id: 3, name: "backend" },
+  { id: 4, name: "school project" },
+];
+
+// 12 14 16 20
+// 24 28 32 40 44 48 52 56 60 64 72 80 96
+
+export const chartData = [
+  { name: "Node.JS", value: 60 },
+  { name: "ASP.NET", value: 56 },
+  { name: "Python", value: 40 },
+  { name: "React", value: 60 },
+  { name: "MySQL", value: 40 },
+  { name: "MongoDB", value: 40 },
+];
+
+export const skills = [
+  {
+    category: "frontend",
+    skills: ["Javascript", "HTML", "CSS", "jQuery", "Sass", "Tailwind", "Bootstrap"],
+  },
+  { category: "backend", skills: ["Node.js", "ASP.NET", "Python", "PHP"] },
+  { category: "database", skills: ["SQL", "MongoDB", "CockroachDB"] },
+  { category: "tool", skills: ["Photoshop", "Premiere", "Finalcut", "Google Ads", "Google Analytics"] },
+];
